8d13bc732e9830e4fca60ffa573cdeed2c149d7e
majikoto <52077584+majikoto@users.noreply.github.com>
2019-06-21 22:47:09 +0800

    game
    Hope you can enjoy it！

---
+++ game/game/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        private int rightCount = 0;
+        private int failedCount = 0;
+        private int inputErrorCount = 0;
+        private Random randomBuilder = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length != 1) return;
+            int inputNumber; 
+            if(int.TryParse(textBox1.Text,out inputNumber))
+            {
+                if(inputNumber<1||inputNumber>3)
+                {
+                    inputErrorCount++;
+                }
+                else if(inputNumber==randomBuilder.Next(1,4))
+                {
+                    rightCount++;
+                }
+                else
+                {
+                    failedCount++;
+                }
+            }
+            else
+            {
+                inputErrorCount++;
+            }
+            textBox2.Text = rightCount.ToString();
+            textBox3.Text = failedCount.ToString();
+            textBox4.Text = inputErrorCount.ToString();
+            textBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
